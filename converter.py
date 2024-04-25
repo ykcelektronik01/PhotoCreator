444ac7a0d1d8dd4ab61f4430bba34e861fc7bfb2
ykorayc <bootcampyuzotuzalti@gmail.com>
2024-04-25 06:48:43 +0300

    Converter is done.

---
+++ converter.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+
+with st.expander("Start Camera"):
+    #Starting the camera
+    camera_image=st.camera_input("Camera")
+
+
+if camera_image:#None veya False olup olmadigini kontrol ediyoruz.
+    #Create a pillow image instance
+    img=Image.open(camera_image)
+
+    #Convert the pillow image to grayscale
+    gray_img=img.convert("L")
+
+    #Render the grayscale image on the webpage
+    st.image(gray_img)
+
